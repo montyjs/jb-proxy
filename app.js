55b34f52a2a0b5cb1403b36f2b3d9b0bf16edbf6
Jordan Boles <jordan@Jordans-MacBook-Pro.local>
2019-06-04 14:14:31 -0600

    configured ports

--- app.js
+++ app.js
@@ -5,9 +5,63 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3333, (err) => {
+// Product Redirecting
+app.get('/images', (req, res) => {
+  res.redirect('http://mg-product-wrapper-dev.us-west-2.elasticbeanstalk.com/images')
+});
+
+app.get('/products', (req, res) => {
+  res.redirect('http://mg-product-wrapper-dev.us-west-2.elasticbeanstalk.com/products')
+});
+
+// Description Redirecting
+app.get('/descriptions', (req, res) => {
+  res.redirect('http://jb-description-dev.us-west-2.elasticbeanstalk.com/descriptions')
+});
+
+app.get('/mensSizes', (req, res) => {
+  res.redirect('http://jb-description-dev.us-west-2.elasticbeanstalk.com/mensSizes')
+});
+
+app.get('/womensSizes', (req, res) => {
+  res.redirect('http://jb-description-dev.us-west-2.elasticbeanstalk.com/womensSizes')
+});
+
+app.get('/euSizes', (req, res) => {
+  res.redirect('http://jb-description-dev.us-west-2.elasticbeanstalk.com/euSizes')
+});
+
+// Reviews Redirecting
+app.get('/reviews', (req, res) => {
+  res.redirect('http://reviews-1.us-west-2.elasticbeanstalk.com/reviews')
+});
+
+
+
+app.listen(3000, err => {
   if (err) {
-    return console.error(err);
+    return console.log(err);
   }
-  console.log('listening on port 3333')
-})
+  console.log('You are connected to the proxy on port 3000')
+});
+
+app.listen(3001, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3001')
+});
+
+app.listen(3002, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3002')
+});
+
+app.listen(3003, err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You are connected to the proxy on port 3003')
+});
